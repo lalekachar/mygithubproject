28ccc945115624f8462e02af9a17815db1b0ba6e
lalekachar <124920736+lalekachar@users.noreply.github.com>
2023-02-10 11:26:05 -0500

    Update firstgitHubClass.java
    Hey Changed to Hello

--- src/Day1GirHub/firstgitHubClass.java
+++ src/Day1GirHub/firstgitHubClass.java
@@ -2,7 +2,7 @@ package Day1GirHub;
 
 public class firstgitHubClass {
     public static void main(String[] args) {
-        System.out.println("hey github");
+        System.out.println("Hello github");
         System.out.println(1+1);
     }
 }
