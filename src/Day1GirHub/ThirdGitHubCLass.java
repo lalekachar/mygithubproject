2406db0eea4a1da4c05346ad990df5b13e93d4f7
lalekachar <lalekachar@gmail.com>
2023-02-13 14:45:42 -0500

    I added new class

--- src/Day1GirHub/ThirdGitHubCLass.java
+++
@@ -1,8 +0,0 @@
-package Day1GirHub;
-
-public class ThirdGitHubCLass {
-    public static void main(String[] args) {
-
-        System.out.println("I am writing this on lalefeauture");
-    }
-}
---
+++ src/Day1GirHub/ThirdGithubClass.java
@@ -0,0 +1,8 @@
+package Day1GirHub;
+
+public class ThirdGithubClass {
+
+    public static void main(String[] args) {
+        System.out.println("I added third class");
+    }
+}
