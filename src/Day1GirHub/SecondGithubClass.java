063c34b0be40205870090b124cdc0c1581df5191
lalekachar <lalekachar@gmail.com>
2023-02-13 14:33:04 -0500

    my second commit,no changes

--- src/Day1GirHub/SecondGithubClass.java
+++ src/Day1GirHub/SecondGithubClass.java
@@ -1,4 +1,10 @@
 package Day1GirHub;
-
-public class SecondGithubClass {
+public class SecondGithubClass{
+    public static void main(String[] args) {
+        System.out.println("GithubprojectTwo");
+    }
 }
+
+
+
+
---
+++ src/Day1GirHub/ThirdGitHubCLass.java
@@ -0,0 +1,8 @@
+package Day1GirHub;
+
+public class ThirdGitHubCLass {
+    public static void main(String[] args) {
+
+        System.out.println("I am writing this on lalefeauture");
+    }
+}
